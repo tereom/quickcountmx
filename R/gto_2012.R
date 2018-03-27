57438ec3e8e6bbfcc1213a9ddf6dd5be445aa72f
tereom <teresa.orti.mancera@gmail.com>
2018-03-27 14:51:02 -0600

    Update data, no NAs in rural

--- R/gto_2012.R
+++ R/gto_2012.R
@@ -6,12 +6,12 @@
 #'
 #' @format A data frame with 6746 rows and 21 variables:
 #' \describe{
-#'   \item{casilla_id}{numeric identifier of the polling station}
-#'   \item{distrito_fed_17, distrito_fed_12}{federal district, the districts
-#'   where redefined in 2017, we include both variables}
-#'   \item{distrito_loc_17, distrito_loc_12}{local district, the districts
-#'   where redefined in 2017, we include both variables}
-#'   \item{seccion}{the section is the miminal electoral geographical unit that
+#'   \item{casilla_id}{Numeric identifier of the polling station.}
+#'   \item{distrito_fed_17, distrito_fed_12}{Federal district, the districts
+#'   where redefined in 2017, we include both variables.}
+#'   \item{distrito_loc_17, distrito_loc_12}{Local district, the districts
+#'   where redefined in 2017, we include both variables.}
+#'   \item{seccion}{The section is the miminal electoral geographical unit that
 #'   contains the polling station.}
 #'   \item{casilla}{Type of polling station, Basic (B) is the
 #'   basic and most common type, Contigua (C) these polling stations arise when
@@ -40,12 +40,14 @@
 #'   is in the western (1) or eastern (2) side of the state.}
 #'   \item{casilla_ex}{Binary variable indicating if the polling station is of
 #'   type Extraordinaria}
+#'   \item{rural}{Binary variable indicating if the polling station is on a
+#'   rural section}
 #' }
 #' @source \url{https://cartografia.ife.org.mx}
 "gto_2012"
 
 # gto <- readr::read_delim("~/Documents/GitHub/ine_cotecora/datos/Resultados electorales estatales/_11_Guanajuato_2012.txt",
-#     "|", escape_double = FALSE, trim_ws = TRUE, locale = locale(encoding = "ISO-8859-1"))
+#     "|", escape_double = FALSE, trim_ws = TRUE, locale = readr::locale(encoding = "ISO-8859-1"))
 # gto_2012 <- gto %>%
 #     dplyr::mutate(
 #         casilla_id = 1:n(),
@@ -54,11 +56,10 @@
 #         distrito_loc_17 = DISTRITO_LOCAL_2017,
 #         distrito_loc_12 = DISTRITO_LOCAL_2012,
 #         seccion = Seccion,
-#         casilla = Casilla,
 #         casilla = dplyr::case_when(
-#             str_detect(Casilla, "[B-C]") ~ "B-C",
-#             str_detect(Casilla, "E") ~ "E",
-#             str_detect(Casilla, "S") ~ "S"
+#             stringr::str_detect(Casilla, "[B-C]") ~ "B-C",
+#             stringr::str_detect(Casilla, "E") ~ "E",
+#             stringr::str_detect(Casilla, "S") ~ "S"
 #         ),
 #         tipo = TIPO_SECCION_21ago_2017,
 #         pri_pvem = PRI + PVEM + `PRI-PVEM`,
@@ -85,9 +86,8 @@
 #             distrito_loc_17 %in% c(1, 3:8, 10:13, 18, 21) ~ 1,
 #             TRUE ~ 2
 #         ),
-#         casilla_ex = (tipo == "E") * 1,
+#         casilla_ex = (casilla == "E") * 1,
+#         rural = dplyr::case_when(tipo == "R" ~ 1, TRUE ~ 0),
 #         ln_total = ifelse(ln == 0, total, ln)
 #     )  %>%
 #     dplyr::select(casilla_id:ln, tamano_md:ln_total)
-
-
