471be436b71c5307fd5d430e7d6e20bc74358f59
tereom <teresa.orti.mancera@gmail.com>
2018-03-27 14:50:13 -0600

    Add Mr p estimation

---
+++ R/mrp_estimation.R
@@ -0,0 +1,40 @@
+#' Compute all postratified estimates
+#'
+#' This function calls \code{\link{mrp_party_estimation}} using all the parties
+#' specified in `...`, once there is an estimation of the number of votes for
+#' each party/candidate it computes the proportion of votes for each one.
+#' @param ... One or more unquoted expressions separated by commas, indicating
+#'   the column names with the votes for each candidate.
+#' @inheritParams mrp_party_estimation
+#' @importFrom magrittr %>%
+#' @importFrom rlang !! !!! :=
+#' @export
+mrp_estimation <- function(data, ..., frac_sample = 1,
+    n_iter = 2000, n_burnin = 500, n_chains = 3, seed = NA){
+    if(is.na(seed)){seed = sample(1:100)}
+    parties <- dplyr::quos(...)
+    data_long <- tidyr::gather(data, party, n_votes, !!!parties)
+    parties_models <- data_long %>%
+        split(.$party) %>%
+        purrr::map(~mrp_party_estimation(., party = n_votes,
+            stratum = distrito_loc_17, frac_sample = frac_sample,
+            n_chains = n_chains, n_iter = n_iter, n_burnin = n_burnin))
+    jags_fits <- purrr::map(parties_models, ~.$fit)
+    votes_all <- purrr::map_df(parties_models, ~.$n_votes) %>%
+        dplyr::mutate(n_sim = 1:n()) %>%
+        tidyr::gather(party, votes, -n_sim) %>%
+        dplyr::group_by(n_sim) %>%
+        dplyr::mutate(
+            total = sum(votes),
+            prop = votes/total
+            )
+    post_summary <- votes_all %>%
+        dplyr::group_by(party) %>%
+        dplyr::summarise(
+            mean_post = 100 * mean(prop),
+            median_post = 100 * median(prop),
+            std_dev_post = 100 * sd(prop)
+            ) %>%
+        dplyr::arrange(mean_post)
+    return(list(jags_fits = jags_fits, post_summary = post_summary))
+}
---
+++ R/mrp_party_estimation.R
@@ -0,0 +1,110 @@
+#' Postratified estimation for the number of votes for a given candidate.
+#'
+#' The function fits a model using the \code{\link[R2jags]{jags}} function and
+#' predicts number of votes for a given candidate in unobserved polling
+#' stations.
+#' @param data A \code{data.frame} with variables: ln_total, region,
+#'   distrito_loc_17, tamano_md, tamano_gd, casilla_ex and the column with
+#'   number of votes for the party.
+#' @param party unquoted variable indicating the column from the data.frame to
+#'   be modeled.
+#' @param stratum If sampling the data, unquoted variable indicating the column
+#'   from the data.frame to be used as strata. The strata will also be used in
+#'   the hierarchical structure of the model.
+#' @param frac_sample If sampling the data, numeric value indicating the fraction
+#'   of the data to sample, the sample is selected using stratified sampling
+#'   with probability proportional to size.
+#' @param n_iter,n_chains,n_burnin Number of iterations, chains and burnin size
+#'  to be used in \code{\link[R2jags]{jags}}.
+#' @param seed Integer value used to set the state of the random number generator.
+#' @return A \code{list} with the object fitted using R2jags::jags and the vector
+#'   of simulated counts per candidate.
+#' @param modelo_jags optional string specifying variations of the jags model.
+#' @details The default model is:
+#'   \deqn{X_k \sim N(n_k \theta_k, n_{k}\sigma^2)}
+#'   \deqn{
+#'   \theta_k=\beta^0 + \beta^{rural}\cdot rural_k + \beta^{tipoSp}\cdot
+#'    tipoSp_k + \\ \beta^{tipoEx}\cdot tipoEx_k + \beta^{tamanoMd}\cdot
+#'    tamanoMd_k + \beta^{tamanoGd}\cdot tamanoGd_k +
+#'    \beta^{distrito}_{distrito(k)} }
+#'    \deqn{\beta_{dl}\sim N(\beta_{region(k)}^{region}, \sigma_{dl}^2)}
+#'
+#' @examples
+#' data("gto_2012")
+#' mrp_party_estimation(gto_2012, party = pan_na, stratum = distrito_loc_17,
+#'   frac_sample = 1, seed = 2212)
+#' @importFrom magrittr %>%
+#' @importFrom rlang !! !!! :=
+#' @export
+mrp_party_estimation <- function(data, party, stratum, frac_sample = 1,
+    n_chains = 3, n_iter = 2000, n_burnin = 500, seed = NA, model_string = NA){
+    stratum_enquo <- dplyr::enquo(stratum)
+    party_enquo <- dplyr::enquo(party)
+    party_name <- dplyr::quo_name(party_enquo)
+
+    if (frac_sample >= 1){
+        data_model <- data
+    } else {
+        data_sample <- select_sample_prop(data, stratum = !!stratum_enquo,
+            frac = frac_sample, seed = seed)
+        data_model <- data %>%
+            dplyr::mutate(!!party_name := ifelse(casilla_id %in%
+                    data_sample$casilla_id, !!party_enquo, NA))
+    }
+
+    data_district <- dplyr::distinct(data, !!stratum_enquo, region) %>%
+        dplyr::arrange(!!stratum_enquo)
+    data_jags <- list(N = nrow(data_model), n = data_model$ln_total,
+        n_regiones = dplyr::n_distinct(data_district$region),
+        n_distritos = nrow(data_district),
+        x = dplyr::pull(data_model, !!party_enquo),
+        rural = data_model$rural,
+        distrito = data_model$distrito_loc_17,
+        tamano_md = data_model$tamano_md,
+        tamano_gd = data_model$tamano_gd, tipo_ex = data_model$casilla_ex,
+        region = data_district$region)
+    if(is.na(model_string)){
+        model_string <-
+            "
+            model{
+                for(k in 1:N){
+                    x[k] ~ dnorm(theta[k], tau / n[k])
+                    theta[k] <- beta_0 + beta_rural * rural[k] +
+                    beta_distrito[distrito[k]] + beta_tamano_md * tamano_md[k] +
+                    beta_tamano_gd * tamano_gd[k] + beta_tipo_ex * tipo_ex[k]
+                }
+                for(j in 1:n_distritos){
+                    beta_distrito[j] ~ dnorm(beta_region[region[j]], tau_distrito)
+                }
+                beta_0 ~ dnorm(0, 0.0002)
+                beta_rural ~ dnorm(0, 0.0002)
+                beta_tamano_md  ~ dnorm(0, 0.0002)
+                beta_tamano_gd  ~ dnorm(0, 0.0002)
+                beta_tipo_ex  ~ dnorm(0, 0.0002)
+                for(i in 1:n_regiones){
+                    beta_region[i] ~ dnorm(0, 0.0002)
+                }
+                sigma ~ dunif(0, 40)
+                tau <- pow(sigma, -2)
+                sigma_distrito ~ dunif(0, 20)
+                tau_distrito <- pow(sigma_distrito, -2)
+            }
+        "
+    }
+
+    temp_file <- tempfile(pattern = "model_string", fileext = ".txt")
+    cat(model_string, file = temp_file)
+    fit_jags <- R2jags::jags(
+        model.file = temp_file,
+        # inits = jags_inits,
+        data = data_jags,
+        parameters.to.save = c("x", "beta_0", "beta_rural", "beta_distrito",
+            "beta_tamano_md", "beta_tamano_gd", "beta_tipo_ex",
+            "beta_region", "sigma", "sigma_distrito"),
+        n.chains = n_chains,
+        n.iter = n_iter,
+        n.burnin = n_burnin
+    )
+    n_votes <- apply(fit_jags$BUGSoutput$sims.list$x, 1, sum)
+    return(list(fit = fit_jags, n_votes = n_votes))
+}
