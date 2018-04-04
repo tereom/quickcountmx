6752c9f0a154622eec279770f59b996b89b17a67
tereom <teresa.orti.mancera@gmail.com>
2018-04-04 10:02:20 -0500

    Data documentation

--- R/nal_2012.R
+++ R/nal_2012.R
@@ -1,8 +1,7 @@
-#' Governor electoral results for the State of Guanaguato 2012.
+#' Presidential electoral results 2012.
 #'
-#' A dataset containing the election results for governor of Guanajuato in 2012,
-#' each row corresponds to a polling station and the variables include the
-#' total counts per party.
+#' A dataset containing the election results for president 2012, each row
+#' corresponds to a polling station.
 #'
 #' @format A data frame with 6746 rows and 21 variables:
 #' \describe{
@@ -11,7 +10,8 @@
 #'   where redefined in 2017, we include both variables.}
 #'   \item{distrito_loc_17, distrito_loc_12}{Local district, the districts
 #'   where redefined in 2017, we include both variables.}
-#'   \item{are}{Electoral Responsability Area identifier for the 2012 election.}
+#'   \item{are}{Electoral Responsability Area identifier for the 2012
+#'   election.}
 #'   \item{seccion}{The section is the miminal electoral geographical unit that
 #'   contains the polling station.}
 #'   \item{casilla}{Type of polling station, Basic (B) is the
@@ -21,7 +21,11 @@
 #'   polling stations are where people can vote outside their asigned polling
 #'   station}
 #'   \item{tipo_seccion}{Section type (in 2017), whether the section of the
-#'   station is rural (R), urban (U) or mixed (M)}
+#'   station is rural (R), urban (U) or mixed (M).}
+#'   \item{rural}{Binary variable indicating if the polling station is on a
+#'   rural section.}
+#'   \item{region}{Numeric variable indicating the state corresponding to the
+#'   polling station.}
 #'   \item{pri_pvem}{Number of votes favoring the parties PRI and/or PVEM.}
 #'   \item{pan_na}{Number of votes favoring the parties PAN and/or Partido
 #'   Nueva Alianza.}
@@ -37,42 +41,54 @@
 #'   \item{tamano_gd}{Binary variable indicating if the popultaion of potential
 #'   voters (size of the nominal list) in the section corresponding to the
 #'   polling station is greater than 5000).}
-#'   \item{region}{Inidcates whether the local district of the polling station
-#'   is in the western (1) or eastern (2) side of the state.}
 #'   \item{casilla_ex}{Binary variable indicating if the polling station is of
 #'   type Extraordinaria}
-#'   \item{rural}{Binary variable indicating if the polling station is on a
-#'   rural section}
 #' }
 #' @source \url{https://cartografia.ife.org.mx}
-"gto_2012"
+"nal_2012"
 
-# gto <- readr::read_delim("~/Documents/GitHub/ine_cotecora/datos/Resultados electorales estatales/_11_Guanajuato_2012.txt",
-#     "|", escape_double = FALSE, trim_ws = TRUE, locale = readr::locale(encoding = "ISO-8859-1"))
-# gto_2012 <- gto %>%
+# nal <- readr::read_delim("~/Documents/GitHub/ine_cotecora/datos/Resultados electorales federales/Computos_Distritales_Presidente_2012.txt",
+#     "|", escape_double = FALSE, trim_ws = TRUE,
+#     locale = readr::locale(encoding = "ISO-8859-1")) %>%
+#     filter(TIPO_CASILLA != "M")
+# nal_estratos <- readr::read_delim("~/Documents/GitHub/ine_cotecora/datos/Presidente2012/Presidente2012_completo.csv",
+#     ",", escape_double = FALSE, trim_ws = TRUE,
+#     locale = readr::locale(encoding = "ISO-8859-1"))
+# nal está mal para Morelos, nal_estrato <- left_join(nal, nal_estratos)
+# View(nal_estrato)
+# glimpse(nal)
+
+# nal_2012 <- nal_estratos %>%
 #     dplyr::mutate(
 #         casilla_id = 1:n(),
 #         distrito_fed_17 = DISTRITO_FEDERAL_2017,
 #         distrito_fed_12 = DISTRITO_FEDERAL_2012,
-#         distrito_loc_17 = DISTRITO_LOCAL_2017,
-#         distrito_loc_12 = DISTRITO_LOCAL_2012,
-#         are = stringr::str_c(distrito_fed_12,
-#             ID_AREA_RESPONSABILIDAD_2E_2012, sep = "-"),
-#         seccion = Seccion,
+#         distrito_loc_17 = Distrito_Local_2017,
+#         # distrito_loc_12 = DISTRITO_LOCAL_2012,
+#         are = stringr::str_c(iD_ESTADO, distrito_fed_12,
+#             ID_AREA_RESPONSABILIDAD_2E__2012, sep = "-"),
+#         seccion = SECCION,
 #         casilla = dplyr::case_when(
-#             stringr::str_detect(Casilla, "[B-C]") ~ "B-C",
-#             stringr::str_detect(Casilla, "E") ~ "E",
-#             stringr::str_detect(Casilla, "S") ~ "S"
+#             stringr::str_detect(TIPO_CASILLA, "[B-C]") ~ "B-C",
+#             stringr::str_detect(TIPO_CASILLA, "E") ~ "E",
+#             stringr::str_detect(TIPO_CASILLA, "S") ~ "S",
+#             TRUE ~ "M"
 #         ),
 #         tipo_seccion = TIPO_SECCION_21ago_2017,
-#         pri_pvem = PRI + PVEM + `PRI-PVEM`,
-#         pan_na = PAN + `PAN-NA` + X13,
-#         prd = PRD,
-#         pt = PT,
-#         mc = MC,
-#         otros = `No registrados` + Nulos,
-#         total = pri_pvem + pan_na + prd + pt + mc + otros,
-#         ln = LN
+#         rural = (CASILLA == 2) * 1,
+#         region = iD_ESTADO,
+#         pri_pvem = CPRI,
+#         pan = CPAN,
+#         panal = CPANAL,
+#         prd_pt_mc = CPRD,
+#         otros = NUM_VOTOS_NULOS + NUM_VOTOS_CAN_NREG,
+#         # pri_pvem = PRI + PVEM + PRI_PVEM,
+#         # pan = PAN,
+#         # panal = PANAL,
+#         # prd_pt_mc = PRD + PT + MC + PRD_PT + PRD_MC + PT_MC + PRD_PT_MC,
+#         # otros = NUM_VOTOS_CAN_NREG + NUM_VOTOS_NULOS,
+#         total = pri_pvem + pan + panal + prd_pt_mc + otros,
+#         ln = LISTA_NOMINAL
 #     ) %>%
 #     dplyr::group_by(seccion) %>%
 #     dplyr::mutate(ln_seccion = sum(ln)) %>%
@@ -85,12 +101,7 @@
 #         ),
 #         tamano_md = (tamano == 2) * 1,
 #         tamano_gd = (tamano == 3) * 1,
-#         region = dplyr::case_when(
-#             distrito_loc_17 %in% c(1, 3:8, 10:13, 18, 21) ~ 1,
-#             TRUE ~ 2
-#         ),
 #         casilla_ex = (casilla == "E") * 1,
-#         rural = dplyr::case_when(tipo_seccion == "R" ~ 1, TRUE ~ 0),
 #         ln_total = ifelse(ln == 0, total, ln)
 #     )  %>%
-#     dplyr::select(casilla_id:ln, tamano_md:ln_total)
+#     dplyr::select(casilla_id:ln, tamano_md:ln_total, estrato)
