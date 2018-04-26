11e2715c5997fe46c6695afe6443646e39f53aaf
Felipe Gonzalez <felipexgonzalez@gmail.com>
2018-04-25 22:54:52 -0500

    Fromatting fixes

--- R/mrp_party_estimation.R
+++ R/mrp_party_estimation.R
@@ -86,24 +86,24 @@ model_bern_t <- function(data_jags, n_chains, n_iter, n_burnin, seed_jags){
   model_string <- "
   model{
     for(k in 1:N){
-    b[k] ~ dbern(p[k])
-    p[k] <- ilogit(beta_0_p + beta_rural_p * rural[k] +
-        beta_rural_tamano_md_p * rural[k] * tamano_md[k] +
-        beta_estrato_raw_p[estrato[k]] + beta_tamano_md_p * tamano_md[k] +
-        beta_tamano_gd_p * tamano_gd[k] + beta_tipo_ex_p * tipo_ex[k] +
-        beta_region_p * region[k])
+      b[k] ~ dbern(p[k])
+      p[k] <- ilogit(beta_0_p + beta_rural_p * rural[k] +
+          beta_rural_tamano_md_p * rural[k] * tamano_md[k] +
+          beta_estrato_raw_p[estrato[k]] + beta_tamano_md_p * tamano_md[k] +
+          beta_tamano_gd_p * tamano_gd[k] + beta_tipo_ex_p * tipo_ex[k] +
+          beta_region_p * region[k])
 
-    mu_mix[k] <- n[k] * theta[k] * b[k]
-    tau_mix[k] <- b[k] * tau[estrato[k]] / n[k] + (1 - b[k]) * 10000
-    nu_mix[k] <- b[k] * nu[estrato[k]] + (1 - b[k]) * 100
+      mu_mix[k] <- n[k] * theta[k] * b[k]
+      tau_mix[k] <- b[k] * tau[estrato[k]] / n[k] + (1 - b[k]) * 10000
+      nu_mix[k] <- b[k] * nu[estrato[k]] + (1 - b[k]) * 100
     
-    x[k] ~ dt(mu_mix[k], tau_mix[k] , nu_mix[k]) T(0, 750)
+      x[k] ~ dt(mu_mix[k], tau_mix[k] , nu_mix[k]) T(0, 750)
 
-    theta[k] <- ilogit(beta_0 + beta_rural * rural[k] +
-    beta_rural_tamano_md * rural[k] * tamano_md[k] +
-    beta_estrato_raw[estrato[k]] + beta_tamano_md * tamano_md[k] +
-    beta_tamano_gd * tamano_gd[k] + beta_tipo_ex * tipo_ex[k] +
-    beta_region * region[k])
+      theta[k] <- ilogit(beta_0 + beta_rural * rural[k] +
+        beta_rural_tamano_md * rural[k] * tamano_md[k] +
+        beta_estrato_raw[estrato[k]] + beta_tamano_md * tamano_md[k] +
+        beta_tamano_gd * tamano_gd[k] + beta_tipo_ex * tipo_ex[k] +
+        beta_region * region[k])
     }
 
     beta_0_p ~ dnorm(0, 0.25)
