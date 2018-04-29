0397389ca498f91ecf8a9eeb0acb655d82fcc685
tereom <teresa.orti.mancera@gmail.com>
2018-04-29 12:50:26 -0500

    Add paraeter description

--- R/calibration.R
+++ R/calibration.R
@@ -1,19 +1,14 @@
 #' Title
 #'
-#' @param data 
-#' @param party 
-#' @param stratum 
-#' @param frac 
-#' @param n_iter 
-#' @param n_burnin 
-#' @param n_chains 
-#' @param seed 
-#' @param cl_cores 
-#' @param n_rep 
-#'
+#' @inheritParams mrp_party_estimation
+#' @param seed An integer vector of length 7 to be send to
+#'     \code{\link[parallel]{clusterSetRNGStream}}.
+#' @param cl_cores Number of cores, parameter is used in
+#'     \code{\link[parallel]{makeCluster}}.
+#' @param n_rep Number of repetitions of sample selection and model fitting.
+#' @param model_string String indicating the model to fit.
 #' @return
 #'
-#' @examples
 #' @importFrom magrittr %>%
 #' @importFrom rlang !! !!! :=
 #' @name calibration
@@ -21,31 +16,29 @@ NULL
 #> NULL
 #' @rdname calibration
 #' @export
-calibration_party <- function(data, party, stratum, frac = 1,
-                              n_iter = 2000, n_burnin = 500, n_chains = 3, seed = NA, cl_cores = 14,
-                              n_rep = 5, model_string = NULL){
-  party_enquo <- dplyr::enquo(party)
-  stratum_enquo <- dplyr::enquo(stratum)
-  data_enquo <- dplyr::enquo(data)
-  actual <- dplyr::pull(data, !!party_enquo) 
-  # set up cluster
-  clust <-  parallel::makeCluster(getOption("cl.cores", cl_cores))
-  parallel::clusterSetRNGStream(clust, seed)
-  parallel::clusterExport(clust, c("frac", "n_iter", "n_burnin", "n_chains", "actual",
-                                   "model_string", "cl_cores",
-                                   "stratum_enquo", "party_enquo"), 
-                          envir = environment())
-  parallel::clusterExport(clust, dplyr::quo_name(data_enquo))
-  parallel::clusterEvalQ(clust, {
-    library(tidyverse)
-    library(quickcountmx)
-  })
+calibration_party <- function(data, party, stratum, frac = 1, n_iter = 2000,
+    n_burnin = 500, n_chains = 3, seed = NA, cl_cores = 14, n_rep = 5,
+    model_string = NULL){
+    party_enquo <- dplyr::enquo(party)
+    stratum_enquo <- dplyr::enquo(stratum)
+    data_enquo <- dplyr::enquo(data)
+    actual <- dplyr::pull(data, !!party_enquo)
+    # set up cluster
+    clust <-  parallel::makeCluster(getOption("cl.cores", cl_cores))
+    parallel::clusterSetRNGStream(clust, seed)
+    parallel::clusterExport(clust, c("frac", "n_iter", "n_burnin", "n_chains",
+        "actual", "model_string", "cl_cores", "stratum_enquo", "party_enquo"),
+        envir = environment())
+    parallel::clusterExport(clust, dplyr::quo_name(data_enquo))
+    parallel::clusterEvalQ(clust, {
+
+    })
   # run replicates
-  clb_party <- parallel::parLapply(clust, 1:n_rep, function(x){
-    counts <- mrp_party_estimation(data, 
-                party = !!party_enquo, frac = frac, stratum = !!stratum_enquo, 
-                n_iter = n_iter, n_burnin = n_burnin, 
-                n_chains = n_chains, seed = NA, 
+    clb_party <- parallel::parLapply(clust, 1:n_rep, function(x){
+        counts <- mrp_party_estimation(data,
+                party = !!party_enquo, frac = frac, stratum = !!stratum_enquo,
+                n_iter = n_iter, n_burnin = n_burnin,
+                n_chains = n_chains, seed = NA,
                 model_string = model_string
                 )
     df <- dplyr::data_frame(n_votes = counts$n_votes, n_sim = x)
@@ -57,8 +50,8 @@ calibration_party <- function(data, party, stratum, frac = 1,
 
 #' @rdname calibration
 #' @export
-calibration_prop <- function(data, ..., stratum, frac = 1, n_iter = 2000, 
-                        n_burnin = 500, n_chains = 3, seed = NA, 
+calibration_prop <- function(data, ..., stratum, frac = 1, n_iter = 2000,
+                        n_burnin = 500, n_chains = 3, seed = NA,
                         cl_cores = 3, n_rep = 5, model_string = NULL){
   stratum_enquo <- dplyr::enquo(stratum)
   parties <- dplyr::quos(...)
@@ -70,18 +63,18 @@ calibration_prop <- function(data, ..., stratum, frac = 1, n_iter = 2000,
   parallel::clusterSetRNGStream(clust, seed)
   parallel::clusterExport(clust, c("frac", "n_iter", "n_burnin", "n_chains", "actual",
                                    "cl_cores", "model_string",
-                                   "stratum_enquo", "parties", "data"), 
+                                   "stratum_enquo", "parties", "data"),
                           envir = environment())
   parallel::clusterEvalQ(clust, {
     library(dplyr)
     library(quickcountmx)
   })
   clb <- parallel::parLapply(clust, 1:n_rep, function(x){
-     mrp <- mrp_estimation(data, !!!parties, frac = frac, 
-                stratum = !!stratum_enquo, n_iter = n_iter, n_burnin = n_burnin, 
+     mrp <- mrp_estimation(data, !!!parties, frac = frac,
+                stratum = !!stratum_enquo, n_iter = n_iter, n_burnin = n_burnin,
                 n_chains = n_chains, seed = NA, parallel = TRUE,
                 model_string = NULL)
-     df <- mrp$post_summary %>% dplyr::left_join(actual) %>% 
+     df <- mrp$post_summary %>% dplyr::left_join(actual) %>%
           dplyr::mutate(n_sim = x)
       df
   })
@@ -93,42 +86,42 @@ calibration_prop <- function(data, ..., stratum, frac = 1, n_iter = 2000,
 #' @export
 summary_calibration_party <- function(calib_run_party, alpha_r = 0.05) {
   cal_summary <- calib_run_party %>% group_by(n_sim) %>%
-    summarise(mean_post = mean(n_votes), 
-              inf = mean(n_votes) - 2*sd(n_votes), 
-              sup = mean(n_votes) + 2*sd(n_votes), 
+    summarise(mean_post = mean(n_votes),
+              inf = mean(n_votes) - 2*sd(n_votes),
+              sup = mean(n_votes) + 2*sd(n_votes),
               actual_votes = actual_votes[1])
   # plot
-  plot_calib <- ggplot(cal_summary, aes(x = factor(n_sim), 
-                                        y = mean_post, ymin = inf, ymax = sup)) + 
+  plot_calib <- ggplot(cal_summary, aes(x = factor(n_sim),
+                                        y = mean_post, ymin = inf, ymax = sup)) +
     geom_hline(aes(yintercept = actual_votes[1]), colour = 'red') +
     geom_hline(aes(yintercept = mean(cal_summary$mean_post))) +
     geom_point() + geom_linerange()
   # coverage report
-  coverage_report <- cal_summary %>% 
+  coverage_report <- cal_summary %>%
     summarise(coverage = mean(inf < actual_votes & sup > actual_votes))
-  
+
   list(plot = plot_calib, coverage = coverage_report)
 }
 
 #' @rdname calibration
 #' @export
 summary_calibration <- function(calib_run, alpha_r = 0.05) {
-    means_party <- calib_run %>% dplyr::group_by(party) %>% 
+    means_party <- calib_run %>% dplyr::group_by(party) %>%
     dplyr::summarise(mean_party = mean(mean_post), prop_votes = mean(prop_votes))
     # plot
-    plot_calib <- ggplot(calib_run, 
+    plot_calib <- ggplot(calib_run,
       aes(x = n_sim, ymin = mean_post - 2 * std_dev_post,
           ymax = mean_post + 2 * std_dev_post)) +
-      geom_linerange(colour='red') + 
+      geom_linerange(colour='red') +
       facet_wrap(~party, scales = 'free_y') +
       geom_hline(data = means_party, aes(yintercept = mean_party), colour ='orange') +
       geom_hline(data = means_party, aes(yintercept = prop_votes), colour ='black')
     # coverage report
-    coverage_report <- calib_run %>% 
-      dplyr::mutate(covered = ((mean_post - 2 * std_dev_post) < prop_votes & 
-        (mean_post + 2 * std_dev_post) > prop_votes)*1) %>% 
-      dplyr::group_by(party) %>% 
-      dplyr::summarise(coverage = mean(covered), 
+    coverage_report <- calib_run %>%
+      dplyr::mutate(covered = ((mean_post - 2 * std_dev_post) < prop_votes &
+        (mean_post + 2 * std_dev_post) > prop_votes)*1) %>%
+      dplyr::group_by(party) %>%
+      dplyr::summarise(coverage = mean(covered),
           n_sims = length(covered),
           mean_error = mean(2*std_dev_post))
   list(plot = plot_calib, coverage = coverage_report)
