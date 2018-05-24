d02fe79a8389a2aca6d139988bb907a889af98e9
tereom <teresa.ortiz.mancera@gmail.com>
2018-05-24 10:09:50 -0500

    Delete garbage from script

--- R/mor_2012.R
+++ R/mor_2012.R
@@ -1,11 +1,11 @@
-# morelos_2012 <- read_delim("DataMorelos/CVE17_Morelos_2012.tsv", 
+# morelos_2012 <- read_delim("DataMorelos/CVE17_Morelos_2012.tsv",
 #     "|", escape_double = FALSE, trim_ws = TRUE)
-# 
+#
 # glimpse(morelos_2012)
-# 
+#
 # plot_missing(morelos_2012)
-# 
-# mor_2012 <- morelos_2012 %>% 
+#
+# mor_2012 <- morelos_2012 %>%
 #     mutate(
 #         casilla_id = 1:n(),
 #         distrito_fed_17 = DISTRITO_FEDERAL_2017,
@@ -23,8 +23,8 @@
 #         tipo_seccion = TIPO_SECCION_21ago_2017,
 #         pri_pvem_pna = PRI + PVEM + PNA + VCC_PRI_PVEM_PNA,
 #         pan = PAN,
-#         prd_pt_mc = PRD + PT + MC + VCC_PRD_PT_MC, 
-#         psd = PSD, 
+#         prd_pt_mc = PRD + PT + MC + VCC_PRD_PT_MC,
+#         psd = PSD,
 #         otros = CANDIDATOS_NO_REGISTRADOS + VOTOS_NULOS,
 #         total = pri_pvem_pna + pan + prd_pt_mc + psd + otros,
 #         ln = LISTA_NOMINAL
@@ -48,13 +48,5 @@
 #         rural = dplyr::case_when(tipo_seccion == "R" ~ 1, TRUE ~ 0),
 #         ln_total = ifelse(ln == 0, total, ln)
 #     )  %>%
-#     dplyr::select(casilla_id:ln, tamano_md:ln_total) 
-# 
-# clust <-  parallel::makeCluster(getOption("cl.cores", no_cores))
-# system.time({
-#     a <- mrp_estimation(mor_2012, pri_pvem_pna:otros, stratum = distrito_loc_17,
-#         frac = 0.087, n_iter = 3000, n_burnin = 1500, n_chains = 2, seed = 888, 
-#         parallel = TRUE, clust = clust)
-#     
-# })
-# parallel::stopCluster(clust)
+#     dplyr::select(casilla_id:ln, tamano_md:ln_total)
+
