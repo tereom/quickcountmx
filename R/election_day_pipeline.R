df5bbb17ed30f7b6670c077a79d47a6b65ef0747
Felipe Gonzalez <felipexgonzalez@gmail.com>
2018-05-31 18:46:18 -0500

    Correct sampling frame loading bug and clean un script

--- R/election_day_pipeline.R
+++ R/election_day_pipeline.R
@@ -16,25 +16,24 @@ process_batch <- function(path_name, file_name){
   new_name <- paste("./procesados/", file_name, "_procesado.rds", sep="")
   data_in <- readr::read_csv(path_name)
   # do processing ########
-  out_df <- data_in
   tipo <- stringr::str_sub(file_name, 8, 9)
   estado_str <- stringr::str_sub(file_name, 10, 11)
   table_frame_in <- dplyr::filter(table_frame, estado == estado_str)
   marco_name <- table_frame_in %>% dplyr::pull(marco)
   candidatos <- table_frame_in$candidatos[[1]]
-  marco <- get(data(marco_name))
+  marco <- get(data(list = marco_name, package = "quickcountmx"))
   # get id
-  print(head(data_in))
+  #print(head(data_in))
   data_out <- data_in %>% dplyr::mutate(id =
     stringr::str_c(ID_ESTADO, SECCION, ID_CASILLA, TIPO_CASILLA, 
                  EXT_CONTIGUA, sep = "-")) %>% 
-    dplyr::select(id, one_of(candidatos)) %>% 
+    dplyr::select(id, dplyr::one_of(candidatos)) %>% 
     dplyr::right_join(marco)
   
   #######################
   saveRDS(data_out, file = new_name)
   df_new <- dplyr::data_frame(archivo = file_name, hora = Sys.time(), 
-                              datos = list(data_out))
+                              datos = list(data_out), originales = list(data_in))
   if(file.exists(all_data_filename)){
     df_prev <- readRDS(all_data_filename)
     df_agg <- dplyr::bind_rows(df_prev, df_new)
