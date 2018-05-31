fbb197dade0c78028c31b01281372b2645304747
tereom <teresa.ortiz.mancera@gmail.com>
2018-05-31 00:08:04 -0500

    Fix typos

--- R/mrp_party_estimation_stan.R
+++ R/mrp_party_estimation_stan.R
@@ -1,6 +1,6 @@
 #' Postratified estimation for the number of votes for a given candidate.
 #'
-#' The function fits a model using the \code{\link[R2jags]{jags}} function and
+#' The function fits a model using the \code{rstan} package and
 #' predicts number of votes for a given candidate in unobserved polling
 #' stations. Optionally the model can be fit with a stratified random sample
 #' of the data, and predict for the population.
@@ -24,7 +24,7 @@
 #' @return A list containing fit object and vector
 #'   the simulated counts for the candidate.
 #' @examples
-#' mrp_party <- mrp_estimation_party_stan(nal_2012, pri_pvem, frac = 0.02,
+#' mrp_party <- mrp_party_estimation_stan(nal_2012, pri_pvem, frac = 0.02,
 #'     stratum = estrato, n_iter = 300, warmup = 150,
 #'     n_chains = 2, seed = 19291)
 #' quantile(mrp_party$y)
@@ -36,8 +36,8 @@
 #' @importFrom rstan sampling
 #' @export
 mrp_party_estimation_stan <- function(data, party, stratum, frac = 1,
-                                 n_iter = 300, warmup = 150, n_chains = 2, 
-                                 seed = NA, 
+                                 n_iter = 300, warmup = 150, n_chains = 2,
+                                 seed = NA,
                                  model_string = NULL, set_strata_na = integer(0)){
   if(is.null(model_string)){
     model_string <- "neg_binomial"
@@ -55,10 +55,10 @@ mrp_party_estimation_stan <- function(data, party, stratum, frac = 1,
       dplyr::mutate(!!party_name := ifelse(casilla_id %in%
                                              data_sample$casilla_id, !!party_enquo, NA))
   }
-  
+
   data_district <- dplyr::distinct(data, !!stratum_enquo, region) %>%
     dplyr::arrange(!!stratum_enquo)
-  x <- dplyr::select(data_sample %>% dplyr::ungroup(), 
+  x <- dplyr::select(data_sample %>% dplyr::ungroup(),
                      tamano_md, tamano_gd, rural) %>% as.matrix
   stratum_vec <- dplyr::pull(data_sample, !!stratum_enquo)
   if(length(set_strata_na) > 0) {
@@ -70,7 +70,7 @@ mrp_party_estimation_stan <- function(data, party, stratum, frac = 1,
       y = dplyr::pull(data_sample, !!party_enquo),
       stratum = dplyr::pull(data_sample, !!stratum_enquo),
       x = x)
-  x_full <- dplyr::select(data %>% dplyr::ungroup(), 
+  x_full <- dplyr::select(data %>% dplyr::ungroup(),
                           tamano_md, tamano_gd, rural) %>% as.matrix
   data_full <- list(N_f = nrow(data), n_f = data$ln_total,
                     n_covariates_f = ncol(x_full),
@@ -79,7 +79,7 @@ mrp_party_estimation_stan <- function(data, party, stratum, frac = 1,
                     y_f = dplyr::pull(data, !!party_enquo),
                     stratum_f = dplyr::pull(data, !!stratum_enquo),
                     x_f = x_full)
-  stan_fit <- sampling(stanmodels[[model_string]], iter = 350, warmup = 150, 
+  stan_fit <- sampling(stanmodels[[model_string]], iter = n_iter, warmup = warmup,
                               chains = n_chains, data= c(data_sample, data_full),
                               init = "0",
                               cores = n_chains, control = list(max_treedepth = 15))
