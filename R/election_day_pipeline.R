329903e065223c6d1abef7273b380310c3b4905d
Felipe Gonzalez <felipexgonzalez@gmail.com>
2018-05-31 21:11:53 -0500

    Fix call to mrp_estimation and add output checking

--- R/election_day_pipeline.R
+++ R/election_day_pipeline.R
@@ -1,5 +1,6 @@
 #' @importFrom magrittr %>%
 #' @importFrom rlang !! !!! :=
+#' @import ggplot2
 #' 
 table_frame <- dplyr::data_frame(estado = c("00","07", "11", "17"),
                marco = c("marco_nal_2018", "marco_chis_2018",
@@ -13,7 +14,7 @@ table_frame <- dplyr::data_frame(estado = c("00","07", "11", "17"),
 #' @export
 process_batch <- function(path_name, file_name){
   all_data_filename = "./procesados/remesas.rds"
-  new_name <- paste0("./procesados/procesado_", file_name)
+  new_name <- paste0("./procesados/procesado_", file_name, ".rds")
   data_in <- readr::read_csv(path_name)
   # do processing ########
   tipo <- stringr::str_sub(file_name, 8, 9)
@@ -35,14 +36,18 @@ process_batch <- function(path_name, file_name){
   
   # run model ###################
   if(estado_str != "00") {
-    fit <- mrp_estimation(data_out, stratum = estrato, n_iter = 300,
-                          n_burnin = 100, n_chains = 1, 
-                          cl_clust = length(candidatos), parallel = TRUE)
+    fit_time <- system.time(
+      fit <- mrp_estimation(data_out, !!! rlang::syms(candidatos),
+                          stratum = estrato, n_iter = 1500,
+                          n_burnin = 500, n_chains = 1, 
+                          mc_cores = length(candidatos), parallel = TRUE)
+    )
   }
-  saveRDS(fit$jags_fit, file = paste0("./procesados/fit_", file_name, ".rds"))
+  print(fit_time)
+  #saveRDS(fit$jags_fit, file = paste0("./procesados/fit_", file_name, ".rds"))
   df_new <- dplyr::data_frame(archivo = file_name, hora = Sys.time(), 
               datos = list(data_out), originales = list(data_in), 
-              post_summary = fit$post_summary)
+              post_summary = list(fit$post_summary))
   if(file.exists(all_data_filename)){
     df_prev <- readRDS(all_data_filename)
     df_agg <- dplyr::bind_rows(df_prev, df_new)
@@ -50,4 +55,37 @@ process_batch <- function(path_name, file_name){
     df_agg <- df_new
    }
    saveRDS(df_agg, all_data_filename)
+   # copy results
+   output <- fit$post_summary
+   #readr::write_csv()
+   # extract deviance and counts simulations
+   log_like_sims <- lapply(1:length(fit$jags_fit), function(i){
+     fit$jags_fit[[i]]$BUGSoutput$sims.list$deviance
+   })
+   names(log_like_sims) <- names(fit$jags_fit)
+   df_ll <- as.data.frame(log_like_sims)
+   df_ll$no_sim <- 1:nrow(df_ll)
+   df_ll_long <- df_ll %>% tidyr::gather(partido, loglike, -no_sim)
+   # vote counts
+   counts_sims <- lapply(1:length(fit$jags_fit), function(i){
+     sims_1 <- apply(fit$jags_fit[[i]]$BUGSoutput$sims.list$x, 1, sum)
+     sims_1
+   })
+   names(counts_sims) <- names(fit$jags_fit)
+   df_cts <- as.data.frame(counts_sims)
+   df_cts$no_sim <- 1:nrow(df_cts)
+   df_cts_long <- df_cts %>% tidyr::gather(partido, conteo_sim, -no_sim)
+   
+   # graphs
+   ggplot2::ggplot(df_ll_long, ggplot2::aes(x=no_sim, y = loglike)) +
+     ggplot2::geom_line(colour = "salmon") +
+     ggplot2::facet_wrap(~partido, ncol=1, scales = "free_y")+
+     ggplot2::theme_bw() + ggplot2::labs(title = "Simulaciones MCMC de devianza")
+   ggplot2::ggsave(paste0("./procesados/deviance-", file_name, ".png"))
+   gr_cts <- ggplot2::ggplot(df_cts_long, ggplot2::aes(x=no_sim, y = conteo_sim, group=partido,
+                                     colour=partido)) +
+     ggplot2::geom_line(colour = "salmon") +
+     ggplot2::theme_bw() + ggplot2::labs(title = "Simulaciones MCMC de conteos totales") +
+     ggplot2::scale_y_log10()
+   ggplot2::ggsave(paste0("./procesados/counts-", file_name, ".png"))
 }
