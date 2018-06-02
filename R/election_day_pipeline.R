59e6719ba287c94b72786219b0ba8c2841fabda6
tereom <teresa.ortiz.mancera@gmail.com>
2018-06-01 19:44:05 -0500

    Change read_csv to read_delim

--- R/election_day_pipeline.R
+++ R/election_day_pipeline.R
@@ -1,21 +1,22 @@
 #' @importFrom magrittr %>%
 #' @importFrom rlang !! !!! :=
 #' @import ggplot2
-#' 
+#'
 table_frame <- dplyr::data_frame(estado = c("00","07", "11", "17"),
                marco = c("marco_nal_2018", "marco_chis_2018",
                 "marco_gto_2018", "marco_mor_2018"),
-               candidatos = 
+               candidatos =
                  list(c("RAC", "JAMK", "AMLO", "JHRC"),
                  c("JAAB", "RAAG", "RCEC", "LFCC", "JAOR"),
                  c("DSRV", "GSG", "FACG", "FRSP", "MBSL"),
                  c("VMCS", "JAMO", "MRGC", "NLMLC", "CBB", "JAVJ", "MRA", "FDH")))
- 
+
 #' @export
 process_batch <- function(path_name, file_name, path_out){
   all_data_filename = paste0(path_out, "/remesas.rds")
   new_name <- paste0(path_out, "/procesado_", file_name, ".rds")
-  data_in <- readr::read_csv(path_name)
+  data_in <- readr::read_delim(path_name, "|", escape_double = FALSE,
+      trim_ws = TRUE)
   print(paste0("datos: ", path_name))
   print(paste0("salidas: ", path_out))
   # do processing ########
@@ -28,28 +29,29 @@ process_batch <- function(path_name, file_name, path_out){
   # get id
   #print(head(data_in))
   data_out <- data_in %>% dplyr::mutate(id =
-    stringr::str_c(ID_ESTADO, SECCION, ID_CASILLA, TIPO_CASILLA, 
-                 EXT_CONTIGUA, sep = "-")) %>% 
+    stringr::str_c(ID_ESTADO, SECCION, ID_CASILLA, TIPO_CASILLA,
+                 EXT_CONTIGUA, sep = "-")) %>%
     dplyr::mutate(OTROS = NULOS + CNR) %>%
-    dplyr::select(id, dplyr::one_of(candidatos)) %>% 
+    dplyr::select(id, dplyr::one_of(candidatos)) %>%
     dplyr::right_join(marco)
-  
+
   #######################
   saveRDS(data_out, file = new_name)
-  
+
   # run model ###################
   if(estado_str != "00") {
     fit_time <- system.time(
       fit <- mrp_estimation(data_out, !!! rlang::syms(candidatos),
                           stratum = estrato, n_iter = 1500,
-                          n_burnin = 500, n_chains = 1, 
+                          n_burnin = 500, n_chains = 1,
                           mc_cores = length(candidatos), parallel = TRUE)
     )
   }
   print(fit_time)
+  print(fit$post_summary)
   #saveRDS(fit$jags_fit, file = paste0("./procesados/fit_", file_name, ".rds"))
-  df_new <- dplyr::data_frame(archivo = file_name, hora = Sys.time(), 
-              datos = list(data_out), originales = list(data_in), 
+  df_new <- dplyr::data_frame(archivo = file_name, hora = Sys.time(),
+              datos = list(data_out), originales = list(data_in),
               post_summary = list(fit$post_summary))
   if(file.exists(all_data_filename)){
     df_prev <- readRDS(all_data_filename)
@@ -78,7 +80,7 @@ process_batch <- function(path_name, file_name, path_out){
    df_cts <- as.data.frame(counts_sims)
    df_cts$no_sim <- 1:nrow(df_cts)
    df_cts_long <- df_cts %>% tidyr::gather(partido, conteo_sim, -no_sim)
-   
+
    # graphs
    ggplot2::ggplot(df_ll_long, ggplot2::aes(x=no_sim, y = loglike)) +
      ggplot2::geom_line(colour = "salmon") +
