7ea379cc31e7215d583a82e97cc82de24c9612de
tereom <teresa.ortiz.mancera@gmail.com>
2018-06-08 19:22:01 -0500

    Fix case with zero observations

--- R/mrp_estimation_stan.R
+++ R/mrp_estimation_stan.R
@@ -14,9 +14,9 @@
 #' @importFrom rstan sampling
 #' @export
 mrp_estimation_stan <- function(data, stratum, 
-                                n_iter = 500, n_warmup = 250, 
-                                n_chains = 1, seed = NA, model_string = NULL,
-                                set_strata_na = integer(0)){
+    n_iter = 500, n_warmup = 250, 
+    n_chains = 1, seed = NA, model_string = NULL,
+    set_strata_na = integer(0)){
     if (is.null(model_string)) {
         model_string <- "neg_binomial_edo"
     }
@@ -29,15 +29,13 @@ mrp_estimation_stan <- function(data, stratum,
         dplyr::left_join(regiones, by = c("iD_ESTADO" = "id_estado"))
     data_split$OTROS <- data_split$CNR + data_split$NULOS
     marco_nal_2018 <- get(data(list = "marco_nal_2018", package = "quickcountmx"))
-    ### CORREGIR #############
-    marco_nal_2018 <- dplyr::filter(marco_nal_2018, !is.na(rural))
     #########################
     marco_split <- marco_nal_2018 %>% 
         dplyr::left_join(regiones, by ="id_estado")
     
     partidos <- c("RAC", "JAMK", "AMLO", "JHRC", "OTROS")
     orden <- expand.grid(partidos = partidos, areas = unique(regiones$area),
-                         stringsAsFactors = FALSE)
+        stringsAsFactors = FALSE)
     orden <- orden %>% dplyr::as_tibble() %>% dplyr::mutate(index = 1:n())
     
     
@@ -46,8 +44,8 @@ mrp_estimation_stan <- function(data, stratum,
     clust <-  parallel::makeCluster(getOption("cl.cores", 35))
     parallel::clusterSetRNGStream(clust, seed)
     parallel::clusterExport(clust, c("data_split", "marco_split", 
-                                     "partidos", "orden", "model_string", "n_chains",
-                                     "n_iter", "n_warmup"), envir = environment())
+        "partidos", "orden", "model_string", "n_chains",
+        "n_iter", "n_warmup"), envir = environment())
     parallel::clusterApply(clust, seq_along(clust), function(i) {
         zz <<- file(sprintf('all-%d.Rout', i), open='wt')
         sink(zz)
@@ -80,34 +78,34 @@ mrp_estimation_stan <- function(data, stratum,
             dplyr::select(id, rural, tamano_md, tamano_gd, region) 
         df_full$region <- factor(df_full$region)
         x_full <- model.matrix(~-1 + rural+tamano_md+tamano_gd+region, data = df_full)
-        x <- x_full[df_full$id %in% data_run$id, ]  
+        x <- x_full[df_full$id %in% data_run$id, , drop = FALSE]  
         print(dim(x))
         print(dim(x_full))
         y <- data_run[, party_name][[1]]
         head(y)
-        data_sample <- list(N = nrow(data_run), n = data_run$ln_total,
-                            n_covariates = ncol(x),
-                            n_strata = length(unique(marco_run$estrato_run)),
-                            y = y,
-                            stratum = dplyr::pull(data_run, estrato_run),
-                            x = x
+        data_sample <- list(N = nrow(data_run), n = as.array(data_run$ln_total),
+            n_covariates = ncol(x),
+            n_strata = length(unique(marco_run$estrato_run)),
+            y = as.array(y),
+            stratum = as.array(dplyr::pull(data_run, estrato_run)),
+            x = x
         )
         
         y_f = marco_run[, party_name][[1]]
         y_f[is.na(y_f)] <- 0
         head(y_f)
         data_full <- list(N_f = nrow(marco_run), n_f = marco_run$ln_total,
-                          n_covariates_f = ncol(x),
-                          in_sample = as.numeric(marco_run$id %in% data_run$id), 
-                          n_strata_f = length(unique(marco_run$estrato_run)),
-                          y_f = y_f,
-                          stratum_f = dplyr::pull(marco_run, estrato_run),
-                          x_f = x_full)
+            n_covariates_f = ncol(x),
+            in_sample = as.numeric(marco_run$id %in% data_run$id), 
+            n_strata_f = length(unique(marco_run$estrato_run)),
+            y_f = y_f,
+            stratum_f = dplyr::pull(marco_run, estrato_run),
+            x_f = x_full)
         stan_fit <- rstan::sampling(quickcountmx:::stanmodels[[model_string]], iter = n_iter,
-                                    warmup = n_warmup, chains = n_chains, 
-                                    data = c(data_sample, data_full),
-                                    init = "0", cores = n_chains, 
-                                    control = list(max_treedepth = 12, adapt_delta = 0.85))
+            warmup = n_warmup, chains = n_chains, 
+            data = c(data_sample, data_full),
+            init = "0", cores = n_chains, 
+            control = list(max_treedepth = 12, adapt_delta = 0.85))
         dplyr::data_frame(y = rstan::extract(stan_fit, 'y_out')[[1]], index = index)
     })
     
@@ -138,12 +136,13 @@ mrp_estimation_stan <- function(data, stratum,
     resumen <- df_2 %>% 
         dplyr::bind_rows(part_1) %>%
         dplyr::group_by(party) %>%
-        dplyr::summarise(mean_post = 100*mean(prop), 
-                         median_post = 100*median(prop),
-                         std_dev_post = 100 * sd(prop)) %>%
+        dplyr::summarise(mean_post = pmin(100, 100*mean(prop)), 
+            median_post = pmin(100, 100*median(prop)),
+            std_dev_post = 100 * sd(prop)) %>%
         dplyr::ungroup() %>%
-        dplyr::mutate(int_l = pmax(0, mean_post - 2* std_dev_post),
-                      int_r = pmin(100, mean_post + 2 * std_dev_post)) %>%
+        dplyr::mutate(
+            int_l = pmin(100, pmax(0, mean_post - 2* std_dev_post)),
+            int_r = pmin(100, mean_post + 2 * std_dev_post)) %>%
         dplyr::arrange(desc(mean_post))
     list(fit = models_party, post_summary = resumen, orden = orden)
 }
