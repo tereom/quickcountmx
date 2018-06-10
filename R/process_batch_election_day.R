cdd1d3b84a32c5246c833ceb90e8b3acbb0ff244
tereom <teresa.ortiz.mancera@gmail.com>
2018-06-10 11:41:41 -0500

    Add id to data_in

--- R/process_batch_election_day.R
+++ R/process_batch_election_day.R
@@ -112,16 +112,16 @@ process_batch <- function(path_name, file_name, path_out, path_results,
         print(paste0("Muestreando fracción ", tam_muestra/nrow(data_in)))
         marco_aux <- marco %>% 
             dplyr::select(id, estrato)
-        data_in <- data_in %>% 
+        data_in <- data_in %>% dplyr::mutate(
+            id = stringr::str_c(iD_ESTADO, SECCION, ID_CASILLA, TIPO_CASILLA,
+                    EXT_CONTIGUA, sep = "-")) %>% 
             dplyr::left_join(marco_aux, by = "id") %>% 
             quickcountmx::select_sample_prop(data_in, stratum = estrato,
                 frac = tam_muestra/nrow(data_in), seed = 187) %>% 
             dplyr::select(-estrato)
     }
     
-    data_out <- data_in %>% dplyr::mutate(id =
-            stringr::str_c(iD_ESTADO, SECCION, ID_CASILLA, TIPO_CASILLA,
-                EXT_CONTIGUA, sep = "-")) %>%
+    data_out <- data_in %>%
         dplyr::mutate(OTROS = NULOS + CNR) %>%
         dplyr::select(id, dplyr::one_of(candidatos)) %>%
         dplyr::right_join(marco)
