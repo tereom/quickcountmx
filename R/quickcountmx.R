50af8beb252d848379e5b5d0391210492f95632c
tereom <teresa.ortiz.mancera@gmail.com>
2018-07-04 12:30:56 -0500

    Improve documentation

--- R/quickcountmx.R
+++ R/quickcountmx.R
@@ -1,10 +1,18 @@
-#' quickcountmx: methodologies to monitor the progress of elections and to 
-#' produce early results based on samples of polling stations for Mexican 
+#' quickcountmx: functions to estimate election results for Mexican 
 #' presidential and governor elections.
 #'
-#' The quickcountmx package provides functions to estimate election results. 
+#' The quickcountmx package provides methodologies to monitor the progress of 
+#' elections and to produce early results based on samples of polling stations.
 #' The package includes the classical survey sample approach of ratio 
 #' estimation, however the main contribution are two bayesian hierarchical 
 #' models built with Stan and JAGS.
 #'
-#' @seealso Useful links: * Description of the Stan model https://jovial-jepsen-cf1904.netlify.com/.
+#' @seealso Useful links: 
+#' \itemize{
+#'   \item Description of the Stan model \url{https://jovial-jepsen-cf1904.netlify.com/.}
+#' }
+#' @docType package
+#' @name quickcountmx
+NULL
+# > NULL
+
